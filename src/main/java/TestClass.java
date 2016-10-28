1ff49b069dc9f498d00c4dde667aa166629b8140
ks489 <ks489@student.le.ac.uk>
2016-10-28 16:43:16 +0100

    Added some test code to explore Graph and Node Classes

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -6,11 +6,34 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("asdf");
+		System.out.println("Starting Normal Initialisation");
 		AssignmentSubmission submission = new AssignmentSubmission("/java/lang/String.class", "toCharArray()[C"); 
 		ClassNode targetClass = AssignmentSubmission.findClassNode("/java/lang/String.class");
 		MethodNode targetMethod = AssignmentSubmission.findMethodNode(targetClass, "toCharArray()[C");
-		System.out.println(targetMethod.instructions.size());
+		//System.out.println(targetMethod.instructions.size());
+		//System.out.println("Ending Normal Initialisation");
+		
+		System.out.println("Printing the cfg graph");
+		System.out.println(submission.cfg.toString());
+		System.out.println("Finished print the graph");
+		util.cfg.Graph graph = submission.cfg;
+		for (util.cfg.Node node : graph.getNodes()) {
+			System.out.println(node.toString());
+			//System.out.println(node.getClass());
+			System.out.println(node.getInstruction().getClass());
+			System.out.println(node.getInstruction().getPrevious());
+			System.out.println(node.getInstruction().getPrevious());
+			System.out.println(node.getInstruction().toString());
+			
+		}
+		
+		
+		//for (Node string : submission.cfg) {
+			
+		//}
+		
+		//System.out.println(submission.targetClassNode.name);
+		//System.out.println(submission.targetMethod.name);
 	}
 
 }
