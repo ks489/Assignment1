7a9958fa2ca12c337dadbc133ee53bdc5a8cc765
ks489 <ks489@student.le.ac.uk>
2016-11-01 09:01:15 +0000

    Added the string.trim method

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -12,9 +12,17 @@ public class TestClass {
 
 	public static void main(String[] args) throws Exception, AnalyzerException {
 		System.out.println("Starting AssignmentSubmission");
-		AssignmentSubmission assignment = new AssignmentSubmission("/java/lang/String.class", "equals(Ljava/lang/Object;)Z");
+		
+		//string equals method
+		//AssignmentSubmission assignment = new AssignmentSubmission("/java/lang/String.class", "equals(Ljava/lang/Object;)Z");
+		
+		//string trim method
+		AssignmentSubmission assignment = new AssignmentSubmission("/java/lang/String.class", "trim()Ljava/lang/String;");
+		
+		
+		
 		System.out.println("Ending AssignmentSubmission");
-
+		System.out.println(assignment.cfg);
 		assignment.isControlDependentUpon();
 		for (util.cfg.Node node : assignment.cfg.getNodes()) {
 			//System.out.println(assignment.isControlDependentUpon(node.getInstruction(), node.getInstruction().getNext()));
